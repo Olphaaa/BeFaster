971c59fd27b435a838fbab2adc8d2581769b7540
Hugo DENIZOT <hugo.denizot@etu.uca.fr>
2021-02-02 09:44:10 +0100

    test

---
+++ Code/Be faster/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Be_faster
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+
+        public Texture2D bgTexture;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            bgTexture = Content.Load<Texture2D>("bg");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(bgTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Code/Be faster/obj/Debug/90/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,11 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "Be faster.dll" comes first in this list. */
+		"Be faster.dll",
+		"MonoGame.Framework.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+	public static String ApiPackageName = "Mono.Android.Platform.ApiLevel_28";
+}
