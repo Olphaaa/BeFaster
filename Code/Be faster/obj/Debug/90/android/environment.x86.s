db2603d4f15a069c5b7cb98414539a2abb282b8e
Olivier BLANC <ol.blanc2000@gmail.com>
2021-03-02 08:43:41 +0100

    [DEV] debut accelerometre

--- Code/Be faster/obj/Debug/90/android/environment.x86.s
+++ Code/Be faster/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"ae37e8a2-1e5a-4e60-9db5-31ff4a1f810d"
+	.asciz	"a06f9a16-8b39-42e4-ac0a-56b0d4e56e88"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
